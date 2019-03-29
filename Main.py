c2cc1f9d8f8b98fde0c3d4d8d39eb7e5f9e67c1b
Seenty <49056915+Seenty@users.noreply.github.com>
2019-03-29 08:16:54 +0100

    Main0.0.1
    Create

---
+++ Main.py
@@ -0,0 +1,23 @@
+############################################## Imports #################################################################
+from tkinter import *
+############################################## Fonctions ##############################################################
+accueil = Tk()
+accueil.geometry("1700x950+0+0")
+accueil.title("Forgoten Heart")
+tex1 = Label (accueil, text="Forgotten Heart", font=("ComicSansMS",90), fg="gold",borderwidth =10 , relief="groove")
+tex1.pack()
+Play = Button (accueil, text= "Jouer", command= accueil.destroy)
+Play.pack()
+accueil.mainloop()
+Welcome= Tk()
+Welcome.geometry ("1700x950+0+0")
+Welcome.title("Forgotten Heart")
+Name=input("Welcome, Choose a name and start playing :")
+tex2a = Label (Welcome, text="Welcome, ", font=("ComicSansMS",20), fg="Black",borderwidth =10)
+tex2a.pack()
+tex2b = Label (Welcome, text=Name, font=("ComicSansMS",20), fg="Gold",borderwidth =10)
+tex2b.pack()
+tex2c = Label (Welcome, text="Are you ready for this adventure ?", font=("ComicSansMS",20), fg="Black",borderwidth =10)
+tex2c.pack()
+Ready = Button (Welcome, text= "I am ready", command = Welcome.destroy)
+Welcome.mainloop()
